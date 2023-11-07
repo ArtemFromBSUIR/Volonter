03a78ae3b34d515328205f512e0c483d590f093a
ArtemFromBSUIR <linkevichartem2003@gmail.com>
2023-11-07 23:01:20 +0300

    first commit

---
+++ Volonter/src/main/java/com/example/Volonter/Controllers/BlogController.java
@@ -0,0 +1,14 @@
+package com.example.Volonter.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+    @GetMapping("/blog")
+    public String blogMain(Model model) {
+        model.addAttribute("title", "Hi java");
+        return "blogMain";
+    }
+}
