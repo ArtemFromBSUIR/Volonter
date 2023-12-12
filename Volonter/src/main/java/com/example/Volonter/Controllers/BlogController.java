6caabc828a81ccb9b71cacf2c69e3ed4f407f387
ArtemFromBSUIR <linkevichartem2003@gmail.com>
2023-12-13 01:44:15 +0300

    working

--- Volonter/src/main/java/com/example/Volonter/Controllers/BlogController.java
+++ Volonter/src/main/java/com/example/Volonter/Controllers/BlogController.java
@@ -5,7 +5,10 @@ import com.example.Volonter.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -20,8 +23,68 @@ public class BlogController {
         return "blogMain";
     }
     @GetMapping("/blog/add")
-    public String blogAdd(Model model) {
+    public String getBlog(Model model) {
         model.addAttribute("title", "");
         return "blogAdd";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogAdd(@ModelAttribute("post")Post post) {
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    public Post getPostById(Long id){
+        Optional<Post> optionalPost = postRepository.findById(id);
+        if(optionalPost.isPresent()){
+            return optionalPost.get();
+        }
+        else{
+            throw new RuntimeException("Post not found!");
+        }
+    }
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("post", getPostById(id));
+        model.addAttribute("title", "Post");
+        return "blogDetails";
+    }
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("post", getPostById(id));
+        model.addAttribute("title", "Post");
+        return "blogEdit";
+    }
+//    @GetMapping("/blog/{id}/edit")
+//    public String blogEdit(@PathVariable(value = "id") long id, Model model) {
+//        if (!postRepository.existsById(id)){
+//            return "redirect:/blog";
+//        }
+//        Optional<Post> post = postRepository.findById(id);
+//        ArrayList<Post> res = new ArrayList<>();
+//        post.ifPresent(res::add);
+//        model.addAttribute("post", res);
+//        return "blogEdit";
+//    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id,@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFullText(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+//    @PostMapping("/blog/{id}/remove")
+//    public String blogPostDelete(@PathVariable(value = "id") long id,@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+//        Post post = postRepository.findById(id).orElseThrow();
+//        postRepository.delete(post);
+//        return "redirect:/blog";
+//    }
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostDelete(@ModelAttribute("post")Post post) {
+        postRepository.delete(post);
+        return "redirect:/blog";
     }
 }
--- Volonter/src/main/java/com/example/Volonter/models/Post.java
+++ Volonter/src/main/java/com/example/Volonter/models/Post.java
@@ -1,18 +1,32 @@
 package com.example.Volonter.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
-
+@Data
 @Entity
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Table(name = "post")
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String title, full_text, anons;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "full_text")
+    private String fullText;
+
+    @Column(name = "anons")
+    private String anons;
+
+    @Column(name = "views")
+    private int views;
 
     public Long getId() {
         return id;
@@ -30,12 +44,12 @@ public class Post {
         this.title = title;
     }
 
-    public String getFull_text() {
-        return full_text;
+    public String getFullText() {
+        return fullText;
     }
 
-    public void setFull_text(String full_text) {
-        this.full_text = full_text;
+    public void setFullText(String fullText) {
+        this.fullText = fullText;
     }
 
     public String getAnons() {
@@ -53,7 +67,4 @@ public class Post {
     public void setViews(int views) {
         this.views = views;
     }
-
-    private int views;
-
 }
